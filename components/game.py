e7204822f276c3ff5586ba101a06aac7a6537af6
Luis Fernando Brasil <luis4756@live.com>
2024-10-05 11:58:08 -0300

    Adiciona som da morte

--- components/game.py
+++ components/game.py
@@ -1,6 +1,7 @@
 import copy
 import os
 import sys
+from time import sleep
 
 import pygame
 
@@ -15,7 +16,7 @@ from systems.collision import Collision
 
 
 class Game:
-    
+
     def __init__(self):
         pygame.init()
         pygame.mixer.music.load('assets\paranoid.mp3')
@@ -26,6 +27,8 @@ class Game:
         self.load_collision_frames()
         self.collision_animations = []
         self.font = pygame.font.Font(None, 36)
+
+        self.explosion_time = None
 
     def load_collision_frames(self):
         for i in range(1, 7):
@@ -72,18 +75,24 @@ class Game:
         pygame.display.set_caption("Dirty Motors")
 
         # Initial square position
-        player_car = Player(1000, 3, AssetsPort.RED_CAR,1, WIDTH, HEIGHT, Vector(100, 300),100)
-        player_car2 = Player(1000, 3, AssetsPort.GREEN_CAR, 2, WIDTH, HEIGHT, Vector(700, 300),100)
+        player_car = Player(1000, 3, AssetsPort.RED_CAR, 1, WIDTH, HEIGHT, Vector(100, 300), 100)
+        player_car2 = Player(1000, 3, AssetsPort.GREEN_CAR, 2, WIDTH, HEIGHT, Vector(700, 300), 100)
         bot = Bot(1000, 2, AssetsPort.PINK_CAR, 0.05, WIDTH, HEIGHT, Vector(200, 200), 1)
         bot2 = Bot(1000, 2, AssetsPort.BLUE_CAR, 0.05,
-                   WIDTH, HEIGHT,  Vector(300, 300), 1)
-        rock_obstacle = Obstacle(50000,AssetsPort.PREDA)
+                   WIDTH, HEIGHT, Vector(300, 300), 1)
+        rock_obstacle = Obstacle(50000, AssetsPort.PREDA)
 
         self.object_list.append(player_car)
         self.object_list.append(player_car2)
 
         # Set up clock to control the frame rate
         clock = pygame.time.Clock()
+
+        gets_hammered_sound = pygame.mixer.Sound(os.path.join('assets/sounds', 'gets-hammered.mp3'))
+        gets_hammered_sound.set_volume(2)
+
+        explosion_sound = pygame.mixer.Sound(os.path.join('assets/sounds', 'explosion.mp3'))
+        explosion_sound.set_volume(0.2)
 
         # Main game loop
         while True:
@@ -95,7 +104,7 @@ class Game:
             # Handle key events for moving the square
             keys = pygame.key.get_pressed()
             time = clock.tick(60) / 1000
-            
+
             # Load the background image
             background_image = pygame.image.load(
                 "assets/sprites/PokeArena.png")
@@ -106,7 +115,7 @@ class Game:
 
             # Renderize the background image
             screen.blit(background_image, (0, 0))
-            
+
             for objeto in self.object_list:
                 if type(objeto) is Bot:
                     objeto.handle_input(time)
@@ -124,20 +133,23 @@ class Game:
                 else:
                     objeto.physics(time)
                     if objeto.health <= 0:
+                        explosion_sound.play()
+                        self.explosion_time = pygame.time.get_ticks()
+
                         collision_position = copy.deepcopy(objeto.position)
                         self.collision_animations.append(CollisionAnimation(self.collision_frames, collision_position))
                         objeto.reset()
-            
-            
+
+            if self.explosion_time and pygame.time.get_ticks() - self.explosion_time >= 500:
+                gets_hammered_sound.play()
+                self.explosion_time = None
             self.draw(screen, time)
-            
+
             # Update the display
             pygame.display.flip()
 
             # Cap the frame rate
             clock.tick(FPS)
-            
-        
 
         # Done! Time to quit.
         pygame.quit()
